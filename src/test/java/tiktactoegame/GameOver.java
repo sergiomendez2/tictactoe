88f776d49fffddfa1cf56e949afb747a52b6e2aa
sergio.mendez2 <sergio.mendez2@telusinternational.com>
2022-07-18 14:54:34 -0600

    removing variables computerscore and humanscore

--- src/test/java/tiktactoegame/GameOver.java
+++ src/test/java/tiktactoegame/GameOver.java
@@ -1,8 +1,7 @@
 package tiktactoegame;
 
 public class GameOver {
-     static  int playerScore = 0;
-    static int computerScore = 0;
+
 
     //we check if the game is over and if it is over we check who won the game!!
     public static boolean isThisGameOver(char [][] gameBoard){
@@ -10,32 +9,32 @@ public class GameOver {
     //Horizontal Winner
     if(gameBoard[0][0] == 'X'&& gameBoard[0][2] == 'X' && gameBoard [0][4] == 'X' ){
         System.out.println("Congrats, the Player Won");
-        playerScore++;
+
         return true;
     }
     if(gameBoard[0][0] == 'O'&& gameBoard[0][2] == 'O' && gameBoard [0][4] == 'O' ){
        System.out.println("Congrats, the Computer Won");
-        computerScore++;
+
         return true;
     }
     if(gameBoard[1][0] == 'X'&& gameBoard[1][2] == 'X' && gameBoard [1][4] == 'X' ){
         System.out.println("Congrats, the Player Won");
-        playerScore++;
+
         return true;
     }
     if(gameBoard[1][0] == 'O'&& gameBoard[1][2] == 'O' && gameBoard [1][4] == 'O' ){
       System.out.println("Congrats, the Computer Won");
-        computerScore++;
+
         return true;
     }
     if(gameBoard[2][0] == 'X'&& gameBoard[2][2] == 'X' && gameBoard [2][4] == 'X' ){
         System.out.println("Congrats, the Player Won");
-        playerScore++;
+
         return true;
     }
     if(gameBoard[2][0] == 'O'&& gameBoard[2][2] == 'O' && gameBoard [2][4] == 'O' ) {
        System.out.println("Congrats, the Computer Won");
-        computerScore++;
+
         return true;
     }
 
@@ -43,57 +42,57 @@ public class GameOver {
 
         if(gameBoard[0][0] == 'X'&& gameBoard[1][0] == 'X' && gameBoard [2][0] == 'X' ){
            System.out.println("Congrats, the Player Won");
-            playerScore++;
+
             return true;
         }
         if(gameBoard[0][0] == 'O'&& gameBoard[1][0] == 'O' && gameBoard [2][0] == 'O' ){
           System.out.println("Congrats, the Computer Won");
-            computerScore++;
+
             return true;
         }
 
         if(gameBoard[0][2] == 'X'&& gameBoard[1][2] == 'X' && gameBoard [2][2] == 'X' ){
            System.out.println("Congrats, the Player Won");
-            playerScore++;
+
             return true;
         }
         if(gameBoard[0][2] == 'O'&& gameBoard[1][2] == 'O' && gameBoard [2][2] == 'O' ){
             System.out.println("Congrats, the Computer Won");
-            computerScore++;
+
             return true;
         }
 
         if(gameBoard[0][4] == 'X'&& gameBoard[1][4] == 'X' && gameBoard [2][4] == 'X' ){
             System.out.println("Congrats, the Player Won");
-            playerScore++;
+
             return true;
         }
         if(gameBoard[0][4] == 'O'&& gameBoard[1][4] == 'O' && gameBoard [2][4] == 'O' ){
            System.out.println("Congrats, the Computer Won");
-            computerScore++;
+
             return true;
         }
 
         //Diagonal Winner
         if(gameBoard[0][0] == 'X'&& gameBoard[1][2] == 'X' && gameBoard [2][4] == 'X' ){
             System.out.println("Congrats, the Player Won");
-            playerScore++;
+
             return true;
         }
         if(gameBoard[0][0] == 'O'&& gameBoard[1][2] == 'O' && gameBoard [2][4] == 'O' ){
             System.out.println("Congrats, the Computer Won");
-            computerScore++;
+
             return true;
         }
 
         if(gameBoard[2][0] == 'X'&& gameBoard[1][2] == 'X' && gameBoard [0][4] == 'X' ){
           System.out.println("Congrats, the Player Won");
-            playerScore++;
+
             return true;
         }
         if(gameBoard[2][0] == 'O'&& gameBoard[1][2] == 'O' && gameBoard [0][4] == 'O' ){
             System.out.println("Congrats, the Computer Won");
-            computerScore++;
+
             return true;
         }
 
